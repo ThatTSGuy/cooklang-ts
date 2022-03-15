51499ef96cb79872b9c7795413c2959254040009
Jack Temko <jacktemko2@gmail.com>
2022-03-14 21:30:00 -0400

    Separated comment RegEx and fixed multiword RegExs

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,14 +1,15 @@
 const metadata = /^>>\s*(?<key>.+?):\s*(?<value>.+)/;
 
-const multiwordIngredient = /@(?<mIngredientName>[^#@]+?){(?<mIngredientQuantity>[^]*?)(?:%(?<mIngredientUnits>[^]+?))?}/;
+const multiwordIngredient = /@(?<mIngredientName>[^@#~[]+?){(?<mIngredientQuantity>[^]*?)(?:%(?<mIngredientUnits>[^}]+?))?}/;
 const singleWordIngredient = /@(?<sIngredientName>[^\s]+)/;
 
-const multiwordCookware = /#(?<mCookwareName>[^#@]+?){(?<mCookwareQuantity>.*?)}/;
+const multiwordCookware = /#(?<mCookwareName>[^@#~[]+?){(?<mCookwareQuantity>.*?)}/;
 const singleWordCookware = /#(?<sCookwareName>[^\s]+)/;
 
 const timer = /~(?<timerName>.*?)(?:{(?<timerQuantity>.*?)(?:%(?<timerUnits>.+))?})/;
 
-const comment = /--.*|\[-.*?-\]/;
+export const comment = /--.*/g;
+export const blockComment = /\s*\[-.*?-\]\s*/g;
 
 export const shoppingListToken = /\[(?<name>.+)\](?<items>[^]*?)(?:\n\n|$)/g;
-export const tokens = new RegExp([metadata, multiwordIngredient, singleWordIngredient, multiwordCookware, singleWordCookware, timer, comment].map(r => r.source).join('|'), 'g');
+export const tokens = new RegExp([metadata, multiwordIngredient, singleWordIngredient, multiwordCookware, singleWordCookware, timer].map(r => r.source).join('|'), 'g');
